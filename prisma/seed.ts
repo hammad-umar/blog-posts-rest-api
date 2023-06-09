a310fa7d4e36ec8053bd945d385db24d761abf4b
Hammad Umar <hammadhawc227@gmail.com>
2023-06-10 03:36:24 +0500

    write seed script to seed categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,38 @@
-console.log('SEEDING...')
+import { PrismaClient } from '@prisma/client'
+import log from '../src/helpers/logger'
+
+const prisma = new PrismaClient()
+
+const main = async (): Promise<void> => {
+  await prisma.category.deleteMany()
+
+  await prisma.category.createMany({
+    data: [
+      {
+        title: 'Technology',
+      },
+      {
+        title: 'Foods',
+      },
+      {
+        title: 'Nature',
+      },
+      {
+        title: 'Animals',
+      },
+      {
+        title: 'Movies',
+      },
+    ],
+  })
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (err) => {
+    log.error(err)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
