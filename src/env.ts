93ce43752dde859293962a422131e11bf369a48c
Hammad Umar <hammadhawc227@gmail.com>
2023-06-10 03:23:40 +0500

    setup planetscale database

--- src/env.ts
+++ src/env.ts
@@ -1,12 +1,10 @@
 import { z } from 'zod'
 import { parseEnv } from 'znv'
-// import log from './helpers/logger'
 
-export const { JWT_EXPIRES, JWT_SECRET, NODE_ENV, PORT } = parseEnv(process.env, {
+export const { JWT_EXPIRES, JWT_SECRET, NODE_ENV, PORT, DATABASE_URL } = parseEnv(process.env, {
   NODE_ENV: z.string(),
   PORT: z.number(),
   JWT_SECRET: z.string(),
   JWT_EXPIRES: z.string(),
+  DATABASE_URL: z.string(),
 })
-
-// log.info([JWT_EXPIRES, JWT_SECRET, NODE_ENV, PORT].join(','))
